428d9f67aacc23b46ca81e6db3f2289bd9e2b877
unknown <1390919155@qq.com>
2024-01-20 16:16:00 +0800

    EP9 materials and physically based rendering (reflection fixed as well)

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -3,6 +3,7 @@
 #include "Walnut/Random.h"
 
 namespace Utils {
+
 	static uint32_t ConvertToRGBA(const glm::vec4& color)
 	{
 		uint8_t r = (uint8_t)(color.r * 255.0f);
@@ -13,13 +14,14 @@ namespace Utils {
 		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
 		return result;
 	}
+
 }
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
 	{
-		//no resize necessary
+		// No resize necessary
 		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
 			return;
 
@@ -34,7 +36,7 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	m_ImageData = new uint32_t[width * height];
 }
 
-void Renderer::Render(const Scene& scene,const Camera& camera)
+void Renderer::Render(const Scene& scene, const Camera& camera)
 {
 	m_ActiveScene = &scene;
 	m_ActiveCamera = &camera;
@@ -44,11 +46,10 @@ void Renderer::Render(const Scene& scene,const Camera& camera)
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
 			glm::vec4 color = PerPixel(x, y);
-			//2Dimention to 1 Demention memory allocate
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
-
 
 	m_FinalImage->SetData(m_ImageData);
 }
@@ -62,47 +63,49 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y)
 	glm::vec3 color(0.0f);
 	float multiplier = 1.0f;
 
-	int bounces = 2;
+	int bounces = 5;
 	for (int i = 0; i < bounces; i++)
 	{
-		Renderer::HitPlayload playload = TraceRay(ray);
-		if (playload.HitDistance < 0.0f)
+		Renderer::HitPlayload payload = TraceRay(ray);
+		if (payload.HitDistance < 0.0f)
 		{
-			glm::vec3 skyColor = glm::vec3(0.0f, 0.0f, 0.0f);
+			glm::vec3 skyColor = glm::vec3(0.6f, 0.7f, 0.9f);
 			color += skyColor * multiplier;
 			break;
 		}
 
 		glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
-		float lightIntensity = glm::max(glm::dot(playload.WorldNormal, -lightDir), 0.0f);
+		float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightDir), 0.0f); // == cos(angle)
 
-		const Sphere& sphere = m_ActiveScene->Spheres[playload.ObjectIndex];
-		glm::vec3 sphereColor = sphere.Albedo;
+		const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
+		const Material& material = m_ActiveScene->Materials[sphere.MaterialsIndex];
+		glm::vec3 sphereColor = material.Albedo;
 		sphereColor *= lightIntensity;
 		color += sphereColor * multiplier;
 
-		multiplier *= 0.7;
+		multiplier *= 0.5f;
 
-		ray.Origin = playload.WorldPosition + playload.WorldNormal * 0.0001f;
-		ray.Direction = glm::reflect(ray.Direction, playload.WorldNormal);
+		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f;
+		ray.Direction = glm::reflect(ray.Direction,
+			payload.WorldNormal + material.Roughness * Walnut::Random::Vec3 (-0.5f, 0.5f)
+		);
 	}
 
-
 	return glm::vec4(color, 1.0f);
-
 }
 
 Renderer::HitPlayload Renderer::TraceRay(const Ray& ray)
 {
-	//(bx^2 + by^2)t^2+(2(axbx + ayby))t +(ax^2 + ay^2 - r^2) = 0
-	//a = ray origin
-	//b = ray direction
-	//r = radius
-	//t = hit distance
+	// (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
+	// where
+	// a = ray origin
+	// b = ray direction
+	// r = radius
+	// t = hit distance
 
 	int closestSphere = -1;
 	float hitDistance = std::numeric_limits<float>::max();
-	for(size_t i = 0; i < m_ActiveScene->Spheres.size(); i++)
+	for (size_t i = 0; i < m_ActiveScene->Spheres.size(); i++)
 	{
 		const Sphere& sphere = m_ActiveScene->Spheres[i];
 		glm::vec3 origin = ray.Origin - sphere.Position;
@@ -111,12 +114,18 @@ Renderer::HitPlayload Renderer::TraceRay(const Ray& ray)
 		float b = 2.0f * glm::dot(origin, ray.Direction);
 		float c = glm::dot(origin, origin) - sphere.Radius * sphere.Radius;
 
-		float discriminent = b * b - 4.0f * a * c;
+		// Quadratic forumula discriminant:
+		// b^2 - 4ac
 
-		if (discriminent < 0.0f)
+		float discriminant = b * b - 4.0f * a * c;
+		if (discriminant < 0.0f)
 			continue;
-		//float t0 = (-b + glm::sqrt(discriminent)) / (2.0f * a);//
-		float closestT = (-b - glm::sqrt(discriminent)) / (2.0f * a);
+
+		// Quadratic formula:
+		// (-b +- sqrt(discriminant)) / 2a
+
+		// float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a); // Second hit distance (currently unused)
+		float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
 		if (closestT > 0.0f && closestT < hitDistance)
 		{
 			hitDistance = closestT;
@@ -132,25 +141,24 @@ Renderer::HitPlayload Renderer::TraceRay(const Ray& ray)
 
 Renderer::HitPlayload Renderer::ClosestHit(const Ray& ray, float hitDistance, int objectIndex)
 {
-
-	Renderer::HitPlayload playload;
-	playload.HitDistance = hitDistance;
-	playload.ObjectIndex = objectIndex;	
+	Renderer::HitPlayload payload;
+	payload.HitDistance = hitDistance;
+	payload.ObjectIndex = objectIndex;
 
 	const Sphere& closestSphere = m_ActiveScene->Spheres[objectIndex];
 
 	glm::vec3 origin = ray.Origin - closestSphere.Position;
-	playload.WorldPosition = origin + ray.Direction * hitDistance;
-	playload.WorldNormal = glm::normalize(playload.WorldPosition);
+	payload.WorldPosition = origin + ray.Direction * hitDistance;
+	payload.WorldNormal = glm::normalize(payload.WorldPosition);
 
-	playload.WorldPosition += closestSphere.Position;
+	payload.WorldPosition += closestSphere.Position;
 
-	return playload;
+	return payload;
 }
 
 Renderer::HitPlayload Renderer::Miss(const Ray& ray)
 {
-	Renderer::HitPlayload playload;
-	playload.HitDistance = -1.0f;
-	return playload;
+	Renderer::HitPlayload payload;
+	payload.HitDistance = -1.0f;
+	return payload;
 }
--- RayTracing/src/Scene.h
+++ RayTracing/src/Scene.h
@@ -3,15 +3,23 @@
 #include <glm/glm.hpp>
 #include <vector>
 
+struct Material
+{
+	glm::vec3 Albedo{ 1.0f };
+	float Roughness = 1.0f;
+	float Matallic = 0.0f;
+};
+
 struct Sphere
 {
 	glm::vec3 Position{0.0f};
 	float Radius = 0.5f;
 
-	glm::vec3 Albedo{1.0f};
+	int MaterialsIndex = 0;
 };
 
 struct Scene
 {
 	std::vector<Sphere> Spheres;
+	std::vector<Material> Materials;
 };
